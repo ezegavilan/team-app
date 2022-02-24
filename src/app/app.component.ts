2c523ff69889baaa1ccb696957576011ac528663
ezegavilan <ezegavilan.developer@gmail.com>
2022-02-24 13:04:18 -0300

    agregar miembros impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'team-app';
+  newMemberName: string = "";
+  members: string[] = [];
+
+  addMember(): void {
+    this.members.push(this.newMemberName);
+
+    console.log(this.members);
+  }
+
+  onInput(member: string): void {
+    this.newMemberName = member;
+  }
 }
